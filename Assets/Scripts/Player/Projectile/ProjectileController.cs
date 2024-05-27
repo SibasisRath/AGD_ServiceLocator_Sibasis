4c800b2b77e88f6a5e1693c6680f045faabe718a
Sibasis Rath <sibasisrath2000@gmail.com>
2024-05-27 14:07:00 +0530

    Player service via service locator done

--- Assets/Scripts/Player/Projectile/ProjectileController.cs
+++ Assets/Scripts/Player/Projectile/ProjectileController.cs
@@ -62,7 +62,7 @@ namespace ServiceLocator.Player.Projectile
         {
             target = null;
             projectileView.gameObject.SetActive(false);
-            PlayerService.Instance.ReturnProjectileToPool(this);
+            GameService.Instance.playerService.ReturnProjectileToPool(this);
         }
 
         private void SetState(ProjectileState newState) => currentState = newState;
